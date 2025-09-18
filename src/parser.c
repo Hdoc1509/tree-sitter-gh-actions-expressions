2496b7d4f60cd4d9fc04f31703ccee94fe9c3d6b
Hdoc1509 <hector.ochoa.dev@gmail.com>
2025-09-17 21:19:05 -0500

    fix: generate

--- src/parser.c
+++ src/parser.c
@@ -2801,7 +2801,7 @@ TS_PUBLIC const TSLanguage *tree_sitter_gh_actions_expressions(void) {
     .max_reserved_word_set_size = 0,
     .metadata = {
       .major_version = 0,
-      .minor_version = 0,
+      .minor_version = 1,
       .patch_version = 0,
     },
   };
