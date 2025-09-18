5dca8722892396fe5ae15c0254315a31bef59be3
Hdoc1509 <Hdoc1509@users.noreply.github.com>
2025-09-18 04:07:55 +0000

    Deploy v0.1.2 5125f7bfdd821a7fc6e09bc389cc46a9192ebfeb

---
+++ grammar.js
@@ -0,0 +1,107 @@
+/**
+ * @file Github Actions expressions grammar for tree-sitter
+ * @author Héctor Ochoa <hector.ochoa.dev@gmail.com>
+ * @license MIT
+ */
+
+/// <reference types="tree-sitter-cli/dsl" />
+// @ts-check
+
+module.exports = grammar({
+  name: "gh_actions_expressions",
+
+  rules: {
+    source: ($) => choice($._if_pair, $._pair),
+
+    _pair: ($) =>
+      seq(
+        /[\w-]+/,
+        ":",
+        repeat1(choice($._ignored_text, $.delimited_expression))
+      ),
+    _ignored_text: () => /[^$]+/,
+
+    _if_pair: ($) =>
+      seq("if", ":", choice($.delimited_expression, $.expression)),
+
+    _evaluation: ($) =>
+      choice($.literal, $.context, $.function_call, $.logical_group),
+
+    expression: ($) =>
+      seq(
+        $._evaluation,
+        repeat(seq($.operator, optional($.not), $._evaluation))
+      ),
+    delimited_expression: ($) =>
+      seq(
+        "${{",
+        seq(
+          optional($.not),
+          $._evaluation,
+          repeat(seq($.operator, optional($.not), $._evaluation))
+        ),
+        "}}"
+      ),
+
+    literal: ($) => choice($.boolean, $.null, $.number, $.string),
+
+    boolean: () => choice("true", "false"),
+
+    null: () => "null",
+
+    _dot: () => ".",
+
+    number: ($) => choice($._int, $._float, $._hex, $._exp),
+    _int: () => /\d+/,
+    _float: ($) => seq(optional("-"), $._int, $._dot, $._int),
+    _hex: () => seq("0x", /[0-9a-f]+/),
+    _exp: ($) => seq($._float, "e", optional("-"), $._int),
+
+    string: ($) =>
+      seq("'", repeat(choice($.string_content, $.scape_sequence)), "'"),
+    string_content: (_) => token(prec(-1, /([^'\\\r\n]|\\(.|\r?\n))+/)),
+    scape_sequence: () => token.immediate("''"),
+    // TODO: scape braces and value's placeholder:
+    // format('{{Hello {0} {1} {2}!}}', 'Mona', 'the', 'Octocat')
+
+    asterisk: () => "*",
+    identifier: () => /[_a-zA-Z][-_a-zA-Z0-9]+/,
+    property_deref: ($) => $._dot,
+
+    context: ($) => seq($.identifier, repeat($.property)),
+    property: ($) =>
+      choice(seq($.property_deref, choice($.identifier, $.asterisk)), $.index),
+    index: ($) => seq("[", choice($.number, $.string), "]"),
+
+    function_call: ($) =>
+      seq(
+        field("function", $.identifier),
+        "(",
+        field("arguments", optional($.arguments)),
+        ")"
+      ),
+    arguments: ($) => seq($._evaluation, repeat(seq(",", $._evaluation))),
+
+    not: () => "!",
+    operator: ($) => choice($.lt, $.le, $.gt, $.ge, $.eq, $.ne, $.and, $.or),
+    lt: () => "<",
+    le: () => "<=",
+    gt: () => ">",
+    ge: () => ">=",
+    eq: () => "==",
+    ne: () => "!=",
+    and: () => "&&",
+    or: () => "||",
+
+    logical_group: ($) =>
+      seq(
+        "(",
+        seq(
+          optional($.not),
+          $._evaluation,
+          repeat(seq($.operator, optional($.not), $._evaluation))
+        ),
+        ")"
+      ),
+  },
+});
